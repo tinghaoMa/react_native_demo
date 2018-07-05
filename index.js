39a2ef2b3ca35aa50a75e1f3a3abb56db848b521
tinghaoma <tinghaoma@sohu-inc.com>
2018-07-05 10:03:05 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+/** @format */
+
+import {AppRegistry} from 'react-native';
+import SetUp from './setUp';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => SetUp);
