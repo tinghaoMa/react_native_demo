7b78f5c0ef9d5d6c39139db0c5e212d521270211
tinghaoma <tinghaoma@sohu-inc.com>
2018-10-10 14:08:21 +0800

    解构赋值传递属性

--- EntryComponent.js
+++ EntryComponent.js
@@ -8,9 +8,11 @@ export default class EntryComponent extends Component {
 
     render() {
         let params={name:'小张',age:201,sex:'男'};
+        let {name,sex}=params;
         return <View>
             <PropsComponent
-                {...params}
+                name={name}
+                sex={sex}
             />
         </View>
     }
