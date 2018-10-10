4ea55e62be1a7bf345ec8872071c8ae5f27c2990
tinghaoma <tinghaoma@sohu-inc.com>
2018-10-10 16:09:25 +0800

    alignSelf相对父布局的位置&&flex 自己的拉伸能力

--- FlexBoxComponent.js
+++ FlexBoxComponent.js
@@ -16,10 +16,10 @@ export default class FlexBoxComponent extends Component {
             <Text style={{width: 100,fontSize: 20, color: 'black', backgroundColor:'white'}}>
                 第一行 justifyContent:'center' 对齐方式
             </Text>
-            <Text style={{width: 100,height:250,fontSize: 20, color: 'blue',backgroundColor:'red'}}>
+            <Text style={{width: 100,height:250,fontSize: 20, color: 'blue',backgroundColor:'red',alignSelf:'flex-end'}}>
                 第二行  alignItems 横向排列x是主轴 y是侧轴 竖向排列x是侧轴 y是主轴
             </Text>
-            <Text style={{width: 100,height:150,fontSize: 20, color: 'red',backgroundColor:'gray'}}>
+            <Text style={{width: 100,height:150,fontSize: 20, color: 'red',backgroundColor:'gray', flex:1}}>
                 第三行 flexWrap:'wrap' 设置换行显示元素 默认值 nowrap
             </Text>
         </View>
