c30694e1aed636807b2ea20ea82c48865dd23508
tinghaoma <tinghaoma@sohu-inc.com>
2018-10-10 16:24:16 +0800

    内边距 外边距

--- FlexBoxComponent.js
+++ FlexBoxComponent.js
@@ -12,11 +12,16 @@ export default class FlexBoxComponent extends Component {
                     marginTop:20,
                     height:800,
                     justifyContent:'center',
-                    alignItems:'stretch'}}>
-            <Text style={{width: 100,fontSize: 20, color: 'black', backgroundColor:'white'}}>
+                    alignItems:'stretch',
+                    borderWidth:4,
+                    borderColor:'red'}}>
+            <Text style={{width: 100,fontSize: 20, color: 'black', backgroundColor:'white', flex:1}}>
                 第一行 justifyContent:'center' 对齐方式
             </Text>
-            <Text style={{width: 100,height:250,fontSize: 20, color: 'blue',backgroundColor:'red',alignSelf:'flex-end'}}>
+            <Text style={{width: 100,height:250,fontSize: 20, color: 'blue',backgroundColor:'red',alignSelf:'flex-end',
+                marginBottom:100,
+                marginLeft:50,
+                padding:20,}}>
                 第二行  alignItems 横向排列x是主轴 y是侧轴 竖向排列x是侧轴 y是主轴
             </Text>
             <Text style={{width: 100,height:150,fontSize: 20, color: 'red',backgroundColor:'gray', flex:1}}>
